0a2c6479ec242ef79bd58cd9b54526a600ea1726
BawornsakS <bs5ax@virginia.edu>
2017-01-16 20:20:20 +0700

    Add comments

--- uninformed_search/Search_test.py
+++ uninformed_search/Search_test.py
@@ -3,10 +3,12 @@ from Graph import Graph
 from Search import *
 from SquareGrid import *
 from implementation import *
+
+#Examples of how you can create a node. You may or may not use this.
 a = Node("A" , 5, 5)
 b = Node("B")
 
-#create dictionary of graph A
+#Create a map called graphA. The data structured used here is a dictionary.
 graphA = Graph()
 graphA.edges = {
     'A': ['B'],
@@ -16,20 +18,24 @@ graphA.edges = {
     'E': ['B']
 }
 
+###This section is for Lab 1
 
-
-g = SquareGrid(30, 15)
-g.walls = DIAGRAM1_WALLS # long list, [(21, 0), (21, 2), ...]
-#draw_grid(g)
-
-#test cases for graph A
+###test cases for graph A
 s = Search.breadth_first_search(graphA,'A','E')
 #d = Search.depth_first_search(graphA, 'A', 'E')
 Search.print_path(s,'A','E')
 #Search.print_path(d,'A','E')
 
 
-cost_so_far = Search.a_star_search(diagram, (1, 4), (7, 8))
-print()
-draw_grid(diagram, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
+
+###This section is for Lab 2.
+
+###How to set up a Square Grid
+#g = SquareGrid(30, 15)
+#g.walls = DIAGRAM1_WALLS # long list, [(21, 0), (21, 2), ...]
+#draw_grid(g)
+
+### A* Algorithm
+#cost_so_far = Search.a_star_search(diagram, (1, 4), (7, 8))
+#draw_grid(diagram, width=3, number=cost_so_far, start=(1, 4), goal=(7, 8))
 
